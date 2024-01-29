6d58d42c94c3d9f5fb623f8897843c2d3d8b660d
danielechiarionù <daniele.chiarion@iisviolamarchesini.edu.it>
2024-01-29 16:34:55 +0100

    Uploading useful java methods for ASCII

---
+++ utilita/ascii.java
@@ -0,0 +1,11 @@
+private static int asciiNum(char x)
+{
+    int ascii = (int)x -65;
+    return ascii;
+}
+
+private static char asciiLet(int x)
+{
+    char ascii = (char)(x+65);
+    return ascii;
+}
