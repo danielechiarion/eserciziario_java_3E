20bc7b589043c4af85d1df49cc62c5f124e504a4
il deniel <150442406+danielechiarion@users.noreply.github.com>
2024-04-10 09:15:12 +0200

    try compareTo method with strings

---
+++ spiegazione/compareToTry.java
@@ -0,0 +1,8 @@
+public class compareToTry {
+    public static void main(String[] args) {
+        String stringa1="elena";
+        String stringa2="francesco";
+
+        System.out.println(stringa1.compareTo(stringa2));
+    }
+}
