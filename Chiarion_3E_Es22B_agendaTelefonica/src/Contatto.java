f86d0976628e8bbd8d34e1b2c9e597f4c6bfdc2a
il deniel <150442406+danielechiarion@users.noreply.github.com>
2024-04-15 11:20:16 +0200

    Completino get/set methods

--- Chiarion_3E_Es22B_agendaTelefonica/src/Contatto.java
+++ Chiarion_3E_Es22B_agendaTelefonica/src/Contatto.java
@@ -1,18 +1,48 @@
 enum tipoContratto{abitazione, cellulare, aziendale, undefined};
 
 public class Contatto {
-    public String nome;
-    public String cognome;
-    public String telefono;
-    public tipoContratto tipo;
+    protected String nome;
+    protected String cognome;
+    protected String telefono;
+    protected tipoContratto tipo;
+	protected double ricarica;
 
-    public String stampa()
+	public Contatto(String nome, String cognome, String telefono, tipoContratto tipo){
+		this.nome=nome;
+		this.cognome=cognome;
+		this.telefono=telefono;
+		this.tipo=tipo;
+		this.ricarica=10; //imposto valore di default
+	}   
+
+	public String stampa()
     {
         /* aggiungo una condizione
         per la tipologia di telefono */
         if(tipo==null)
             tipo=tipoContratto.undefined; //assegno il valore undefined
 
-        return String.format("Nome: %s\tCognome: %s\nTelefono: %s,\ttipo: %s\n", nome, cognome, telefono, tipo.toString());
+        return String.format("Nome: %s\tCognome: %s\nTelefono: %s,\ttipo: %s\n%.2f€\n", nome, cognome, telefono, tipo.toString(), ricarica);
     }
+	
+	/* metod get/set */
+	public String getNome(){
+		return this.nome;
+	}
+	public String getCognome(){
+		return this.cognome;
+	}
+	public String getTelefono(){
+		return this.telefono;
+	}
+	
+	public void setNome(String nome){
+		this.nome=nome;
+	}
+	public void setCognome(String cognome){
+		this.cognome=cognome;
+	}
+	public void setTipo(tipoContratto tipo){
+		this.tipo=tipo;
+	}
 }
