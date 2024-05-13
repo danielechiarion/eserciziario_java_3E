fa5d9259b4de13102075cb64d59b2787175218bc
Daniele Chiarion <daniele.chiarion@rois012001.onmicrosoft.com>
2024-05-13 08:09:34 +0200

    Remove utility files

--- utilita/ClrScr.java
+++
@@ -1,7 +0,0 @@
-private static void ClrScr() {
-    try {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
